dfe670141b77fdb913519160548f4cb1f87b16d6
sanjaymundhra <sanjaymundhra1988@gmail.com>
2016-12-24 19:43:01 +0530

    AngularJs Controller

---
+++ javascript/angularJsControllers.js
@@ -0,0 +1,6 @@
+/*AngularJs Controller 'angularController' */
+var app = angular.module('angularApp',[]);
+app.controller('angularController',function($scope){
+	$scope.firstName = "sanjay";
+	$scope.lastName = "Maheshwari";
+});
